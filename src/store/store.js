9891dd2c815a799909d8fe33ee747b6aee165a02
UtkarshSiddhpura <utkarshsiddhpura09876@gmail.com>
2022-08-29 12:59:20 +0530

    Adding redux-persist and react-dev-tools

--- src/store/store.js
+++ src/store/store.js
@@ -3,8 +3,32 @@ import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
 
-const middleWares = [logger];
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(
+	Boolean
+);
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+const composedEnhancer =
+	(process.env.NODE_ENV !== "production" &&
+		window &&
+		window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+	compose;
+
+const composedEnhancers = composedEnhancer(applyMiddleware(...middleWares));
+
+const persistConfig = {
+	key: "root",
+	storage,
+	blacklist: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(
+	persistedReducer,
+	undefined,
+	composedEnhancers
+);
+export const persistor = persistStore(store);
