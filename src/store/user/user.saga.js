d589c3fb6b417628b7fc5d2d51bde41e551c923e
UtkarshSiddhpura <90092555+UtkarshSiddhpura@users.noreply.github.com>
2023-03-23 15:12:21 +0530

    Update user.saga.js with sign-in with Google Redirect

--- src/store/user/user.saga.js
+++ src/store/user/user.saga.js
@@ -14,7 +14,7 @@ import {
 	getCurrentUser,
 	createUserFromAuth,
 	signInUserFromEmailFromAuth,
-	signInWithGooglePopup,
+	signInWithGoogleRedirect,
 	createUserFromEmailFromAuth,
 	signOutUser,
 } from "../../utils/firebase/firebase.utils";
@@ -46,7 +46,7 @@ export function* isUserAuthenticated() {
 
 export function* signInWithGoogle() {
 	try {
-		const { user } = yield call(signInWithGooglePopup);
+		const { user } = yield call(signInWithGoogleRedirect);
 		console.log(user);
 
 		yield put(signInSuccess(user));
